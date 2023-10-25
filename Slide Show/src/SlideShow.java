cc0cce1adce41d714bfe81e369db31ed7e6cbfce
nwyrwas <82687997+nwyrwas@users.noreply.github.com>
2023-10-24 21:48:12 -0500

    Add files via upload

---
+++ Slide Show/src/SlideShow.java
@@ -0,0 +1,172 @@
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.EventQueue;
+import java.awt.FlowLayout;
+import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.Color;
+
+public class SlideShow extends JFrame {
+
+	//Declare Variables
+	private JPanel slidePane;
+	private JPanel textPane;
+	private JPanel buttonPane;
+	private CardLayout card;
+	private CardLayout cardText;
+	private JButton btnPrev;
+	private JButton btnNext;
+	private JLabel lblSlide;
+	private JLabel lblTextArea;
+
+	/**
+	 * Create the application.
+	 */
+	public SlideShow() throws HeadlessException {
+		initComponent();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initComponent() {
+		//Initialize variables to empty objects
+		card = new CardLayout();
+		cardText = new CardLayout();
+		slidePane = new JPanel();
+		textPane = new JPanel();
+		textPane.setBackground(Color.BLUE);
+		textPane.setBounds(5, 470, 790, 50);
+		textPane.setVisible(true);
+		buttonPane = new JPanel();
+		btnPrev = new JButton();
+		btnNext = new JButton();
+		lblSlide = new JLabel();
+		lblTextArea = new JLabel();
+
+		//Setup frame attributes
+		setSize(800, 600);
+		setLocationRelativeTo(null);
+		setTitle("Top 5 Destinations SlideShow");
+		getContentPane().setLayout(new BorderLayout(10, 50));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		//Setting the layouts for the panels
+		slidePane.setLayout(card);
+		textPane.setLayout(cardText);
+		
+		//logic to add each of the slides and text
+		for (int i = 1; i <= 5; i++) {
+			lblSlide = new JLabel();
+			lblTextArea = new JLabel();
+			lblSlide.setText(getResizeIcon(i));
+			lblTextArea.setText(getTextDescription(i));
+			slidePane.add(lblSlide, "card" + i);
+			textPane.add(lblTextArea, "cardText" + i);
+		}
+
+		getContentPane().add(slidePane, BorderLayout.CENTER);
+		getContentPane().add(textPane, BorderLayout.SOUTH);
+
+		buttonPane.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 10));
+
+		btnPrev.setText("Previous");
+		btnPrev.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				goPrevious();
+			}
+		});
+		buttonPane.add(btnPrev);
+
+		btnNext.setText("Next");
+		btnNext.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				goNext();
+			}
+		});
+		buttonPane.add(btnNext);
+
+		getContentPane().add(buttonPane, BorderLayout.SOUTH);
+	}
+
+	/**
+	 * Previous Button Functionality
+	 */
+	private void goPrevious() {
+		card.previous(slidePane);
+		cardText.previous(textPane);
+	}
+	
+	/**
+	 * Next Button Functionality
+	 */
+	private void goNext() {
+		card.next(slidePane);
+		cardText.next(textPane);
+	}
+
+	/**
+	 * Method to get the images
+	 */
+	//Added new photos to address the concerns made by the Product Owner
+	//New travel locations focusing on detox/wellness
+	private String getResizeIcon(int i) {
+		String image = ""; 
+		if (i==1){
+			image = "<html><body><img width= '800' height='500' src='" + getClass().getResource("/resources/image1.jpg") + "'</body></html>";
+		} else if (i==2){
+			image = "<html><body><img width= '800' height='500' src='" + getClass().getResource("/resources/image2.jpg") + "'</body></html>";
+		} else if (i==3){
+			image = "<html><body><img width= '800' height='500' src='" + getClass().getResource("/resources/image3.jpg") + "'</body></html>";
+		} else if (i==4){
+			image = "<html><body><img width= '800' height='500' src='" + getClass().getResource("/resources/image4.jpg") + "'</body></html>";
+		} else if (i==5){
+			image = "<html><body><img width= '800' height='500' src='" + getClass().getResource("/resources/image5.jpg") + "'</body></html>";
+		}
+		return image;
+	}
+	
+	/**
+	 * Method to get the text values
+	 */
+	//Added new photos to address the concerns made by the Product Owner
+		//New travel locations focusing on detox/wellness
+	private String getTextDescription(int i) {
+		String text = ""; 
+		if (i==1){
+			text = "<html><body><font size='5'>#1 The Ranch Mailbu.</font> <br>California’s most acclaimed wellness resorts, and assists guests in recalibrating their mind, body and spirit through an intensive week long endurance, wellness and nutrition program.</body></html>";
+		} else if (i==2){
+			text = "<html><body>#2 Kamalaya. </font> <br>On the lush Thai island Koh Samui, combines Eastern and Western medical treatments for as full a reset as you want.</body></html>";
+		} else if (i==3){
+			text = "<html><body>#3 Ananda in The Himilayas</font> <br>Ananda Spa in the Himalayas is located in the region that gave birth to Ayurveda, the process of identifying and correcting imbalance in the main bio energies</body></html>";
+		} else if (i==4){
+			text = "<html><body>#4 BodyHoliday Saint Lucia</font> <br>All-Inclusive Wellbeing in the Caribbean. bodyholiday st lucia</body></html>";
+		} else if (i==5){
+			text = "<html><body>#5 Golden Door</font> <br>The Golden Door is meant to be place for detoxing both mind and body, and the food is accordingly local, organic, and healthy</body></html>";
+		}
+		return text;
+	}
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				SlideShow ss = new SlideShow();
+				ss.setVisible(true);
+			}
+		});
+	}
+}
